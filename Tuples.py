2fe27498c70797b79200ad78ab5bb37ef8a8ef57
thontiyugender <thonti.yugender012@gmail.com>
2025-08-26 14:21:33 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,17 @@
+t=("1","yugender","mana","snehithudusa","131","917")
+
+print()
+print(t)
+print()
+print("the type of t is:",type(t))
+print()
+print(id(t))
+print(t[3]) 
+ 
+# t.append(55)  ----APPEND IS NOT GONNA PERFORM IN TUPLES----   
+
+ # t[4]=99.5       ====not gonna perform =====
+
+s="000000"
+s[4]="x"
+
---
+++ lists.py
@@ -0,0 +1,22 @@
+l=[1,2,3,4,5,6]
+
+print(type(l))
+print(l)
+
+l.append(100)
+print(l)
+
+print("the first item is",l[0])
+print("The second index is",l[1])
+print("The third index is",l[2])
+print("the fourth index is:",l[3])
+print("the last item is",l[-1])
+print("the second to last item is",l[-2])
+
+l[2] ='skip'
+l[3]="a"
+l[4]="few"
+print()
+print(l)
+print()
+
