9f3a34a109dd54055aaf720540902f6be692e738
Thonti Yugender <thonti.yugender012@gmail.com>
2025-08-27 12:04:51 +0530

    Add files via upload

---
+++ dictonaries.py
@@ -0,0 +1,40 @@
+# Dictonaries store values in key-value pairs.
+
+# If you have the key you can get the value.
+
+# the syntax for accessing and assigning values is the same for lists.
+
+
+d=dict()
+d[5]="five"
+d[2]="two"
+d["pi"]=3.1415926
+
+print(d)
+print(d["pi"])
+print(type(d))
+
+
+
+#   keys can be  different types.
+# but they must be immutable types such as atomic types,tuples or strings.
+
+'''dictionaries are alo known as maps,mappings or hashtables'''
+
+# a dictionary doesnt have a fixed order..
+
+
+"if you assign the key that's not in the dictionary ."
+"it simply creates new item."
+"if you try to access the key that's not in the dictionary."
+"you wil get a key error."
+
+
+#tuples() are not callable...lists[] are used... 
+
+D={"a":"one","b":"two"}
+# D["c"]
+
+
+print(D["a"])
+
